cfc94fa6ce704650eea1cd6943291a1f5d544edc
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-11-22 10:52:57 +0000

    Adding feed data.

--- src/test/scala/PerformanceSimulation.scala
+++ src/test/scala/PerformanceSimulation.scala
@@ -1,5 +1,3 @@
-import java.util.concurrent.ThreadLocalRandom
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
@@ -9,27 +7,32 @@ class PerformanceSimulation extends Simulation {
 
     val httpProtocol = http
 
-    val json =
+    val product =
         """
           {
             "id" : null,
             "cartEventType" : "ADD_ITEM",
             "userId" : 1,
             "productId" : "SKU-12464",
-            "quantity" : 2
+            "quantity" : 1
           }
         """
 
+    val users = csv("user_credentials.csv").queue
+
+    val hostIp = "http://10.100.201.102"
+
     val scn = scenario("Performance simulation")
-        .repeat(10) {
+        .repeat(1) {
             pause(2 seconds)
+                feed(users)
                 .exec(
                     http("Perform login")
-                        .post("http://localhost:8181/uaa/oauth/token")
+                        .post(hostIp + ":8181/uaa/oauth/token")
                         .header("Authorization", "Basic YWNtZTphY21lc2VjcmV0")
                         .header("Content-Type", "application/x-www-form-urlencoded")
-                        .formParam("username", "user")
-                        .formParam("password", "password")
+                        .formParam("username", "${username}")
+                        .formParam("password", "${password}")
                         .formParam("grant_type", "password")
                         .formParam("scope", "openid")
                         .formParam("client_id", "acme")
@@ -40,17 +43,18 @@ class PerformanceSimulation extends Simulation {
                 .pause(2 seconds)
                 .exec(
                     http("Add items to the cart")
-                        .post("http://localhost:8957/v1/events")
+                        .post(hostIp + ":8957/v1/events")
                         .header("Authorization", "Bearer ${accessToken}")
                         .header("Content-Type", "application/json")
-                        .body(StringBody(json)).asJSON
+                        .body(StringBody(product)).asJSON
                         .check(status is 200)
                 )
                 .pause(2 seconds)
                 .exec(
                     http("Perform the checkout process")
-                        .post("http://localhost:8957/v1/checkout")
+                        .post(hostIp + ":8957/v1/checkout")
                         .header("Authorization", "Bearer ${accessToken}")
+                        .header("Content-Type", "application/json")
                         .check(status is 200)
                 )
         }
--- src/test/scala/PerformanceSimulation2.scala
+++ src/test/scala/PerformanceSimulation2.scala
@@ -7,55 +7,54 @@ class PerformanceSimulation2 extends Simulation {
 
     val httpProtocol = http
 
-    val json =
+    val product =
         """
           {
             "id" : null,
             "cartEventType" : "ADD_ITEM",
             "userId" : 1,
             "productId" : "SKU-12464",
-            "quantity" : 2
+            "quantity" : 1
           }
         """
 
-    val scn = scenario("test")
-        .exec(
-            http("Perform login")
-                .post("http://localhost:8181/uaa/oauth/token")
-                .header("Authorization", "Basic YWNtZTphY21lc2VjcmV0")
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .formParam("username", "user")
-                .formParam("password", "password")
-                .formParam("grant_type", "password")
-                .formParam("scope", "openid")
-                .formParam("client_id", "acme")
-                .formParam("client_secret", "acmesecret")
-                .check(jsonPath("$.access_token").exists.saveAs("accessToken"))
-                .check(status is 200)
-        )
-        .pause(2 seconds)
-        .exec(
-            http("Add items to the cart")
-                .post("http://localhost:8957/v1/events")
-                .header("Authorization", "Bearer ${accessToken}")
-                .header("Content-Type", "application/json")
-                .body(StringBody(json)).asJSON
-                .check(status is 200)
-        )
-        .pause(2 seconds)
-        .exec(
-            http("Perform the checkout process")
-                .post("http://localhost:8957/v1/checkout/orchestrated")
-                .header("Authorization", "Bearer ${accessToken}")
-                .check(status is 200)
-        )
-        /*.pause(2 seconds)
-        .exec(
-            http("Perform the orchestrated checkout process")
-                .post("http://localhost:8957/v1/checkout")
-                .header("Authorization", "Bearer ${accessToken}")
-                .check(status is 200)
-        )*/
+    val users = csv("user_credentials.csv").queue
+
+    val scn = scenario("Performance simulation")
+        .repeat(30) {
+            pause(10 seconds)
+                feed(users)
+                .exec(
+                    http("Perform login")
+                        .post("http://localhost:8181/uaa/oauth/token")
+                        .header("Authorization", "Basic YWNtZTphY21lc2VjcmV0")
+                        .header("Content-Type", "application/x-www-form-urlencoded")
+                        .formParam("username", "${username}")
+                        .formParam("password", "${password}")
+                        .formParam("grant_type", "password")
+                        .formParam("scope", "openid")
+                        .formParam("client_id", "acme")
+                        .formParam("client_secret", "acmesecret")
+                        .check(jsonPath("$.access_token").exists.saveAs("accessToken"))
+                        .check(status is 200)
+                )
+                .pause(2 seconds)
+                .exec(
+                    http("Add items to the cart")
+                        .post("http://localhost:8957/v1/events")
+                        .header("Authorization", "Bearer ${accessToken}")
+                        .header("Content-Type", "application/json")
+                        .body(StringBody(product)).asJSON
+                        .check(status is 200)
+                )
+                .pause(2 seconds)
+                .exec(
+                    http("Perform the checkout process")
+                        .post("http://localhost:8957/v1/checkout/orchestrated")
+                        .header("Authorization", "Bearer ${accessToken}")
+                        .check(status is 200)
+                )
+        }
 
     setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 
